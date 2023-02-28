0544a0a95c0a82ad2d026993ab300b9bb0aeb1ba
Tartynn <nathan.dely@gmail.com>
2023-02-28 13:53:12 +0100

    Read the selection, trying to read the input in RT

--- PhonoWritterWord/src/taskpane/taskpane.js
+++ PhonoWritterWord/src/taskpane/taskpane.js
@@ -4,12 +4,14 @@
  */
 
 /* global document, Office, Word */
-
+//const PONCTUATION = [" ", ",", ".", "!", "?"];
+//var currentWord = "";
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    document.getElementById("selection").onclick = select;
+    document.getElementById("lastWord").onclick = lastWord;
     /*
     // Get the current Word document object
     // Get the current selection object
@@ -22,17 +24,39 @@ Office.onReady((info) => {
   }
 });
 
-export async function run() {
-  return Word.run(async (context) => {
-    /**
-     * Insert your Word code here
-     */
-    // insert a paragraph at the end of the document.
-    //context.document.body.paragraphs.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
+export function lastWord() {
+  return Word.run(function (context) {
+    /*
+    // Retrieve the first text range in the document
+    var textRange = context.document.body.getRange("Whole");
 
+    // Create a new binding for the text range
+    var binding = context.document.bindings;
+
+    // Set up an event handler for the BindingDataChanged event
+    binding.addHandlerAsync(Office.EventType.BindingDataChanged, onDataChanged);
+
+    // Function to handle the BindingDataChanged event
+    function onDataChanged(eventArgs) {
+      // Get the binding data
+      var bindingData = binding.getDataAsync(function (result) {
+        if (result.status === "succeeded") {
+          // Log the binding data to the console
+          console.log("User input: " + result.value);
+        }
+      });
+    }
+    // Synchronize the document state with the Office.js runtime
+    return context.sync();*/
+  });
+}
+
+export async function select() {
+  return Word.run(async function (context) {
+    var selection = context.document.getSelection();
+    context.load(selection);
+    var html = selection.getHtml();
     await context.sync();
+    document.getElementById("mySelection").innerHTML = html.value; //Get the selected text in HTML
   });
 }
