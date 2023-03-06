61425f2f02d45acc80bc41ab5cbe2d093555ab71
Gaétan Mottet <gaetan.mottet@students.hevs.ch>
2023-03-06 10:03:41 +0100

    Word selection + replacement

--- PhonoWritterWord/src/taskpane/taskpane.js
+++ PhonoWritterWord/src/taskpane/taskpane.js
@@ -1,62 +1,102 @@
 /*
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
+ *
+ * Tutorial's link : https://learn.microsoft.com/fr-fr/office/dev/add-ins/tutorials/word-tutorial
  */
 
 /* global document, Office, Word */
-//const PONCTUATION = [" ", ",", ".", "!", "?"];
-//var currentWord = "";
+
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("selection").onclick = select;
-    document.getElementById("lastWord").onclick = lastWord;
-    /*
-    // Get the current Word document object
-    // Get the current selection object
-    const selection = document.getSelection();
-    // Get the range object for the current paragraph
-    const paragraphRange = selection.getRange("Paragraph");
-    // Get the text of the current paragraph
-    Console.log(`The current paragraph is: ${paragraphRange}`);
-    */
+    // document.getElementById("run").onclick = run;
+
+    // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
+    if (!Office.context.requirements.isSetSupported("WordApi", "1.3")) {
+      console.log("Sorry. The tutorial add-in uses Word.js APIs that are not available in your version of Office.");
+    }
+
+    // Assign event handlers and other initialization logic.
+
+    // Add event handler for document selection change event
+    Office.context.document.addHandlerAsync(
+      Office.EventType.DocumentSelectionChanged,
+      detectSelection,
+      function (result) {
+        if (result.status !== Office.AsyncResultStatus.Succeeded) {
+          console.log("Could not add event handler: " + result.error.message);
+        }
+      }
+    );
+
+    // Office.context.document.addHandlerAsync(
+    //   Office.EventType.ContentControlAdded,
+    //   detectWrittenText,
+    //   function (result) {
+    //     if (result.status !== Office.AsyncResultStatus.Succeeded) {
+    //       console.log("Could not add event handler: " + result.error.message);
+    //     }
+    //   }
+    // );
   }
 });
 
-export function lastWord() {
-  return Word.run(function (context) {
-    /*
-    // Retrieve the first text range in the document
-    var textRange = context.document.body.getRange("Whole");
-
-    // Create a new binding for the text range
-    var binding = context.document.bindings;
-
-    // Set up an event handler for the BindingDataChanged event
-    binding.addHandlerAsync(Office.EventType.BindingDataChanged, onDataChanged);
-
-    // Function to handle the BindingDataChanged event
-    function onDataChanged(eventArgs) {
-      // Get the binding data
-      var bindingData = binding.getDataAsync(function (result) {
-        if (result.status === "succeeded") {
-          // Log the binding data to the console
-          console.log("User input: " + result.value);
-        }
-      });
+function detectSelection() {
+  var selection = Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, function (result) {
+    if (result.status !== Office.AsyncResultStatus.Succeeded) {
+      console.log("Error: " + result.error.message);
+    } else {
+      var selectedText = result.value;
+      document.getElementById("userText").innerHTML = selectedText;
+      var listContainer = document.getElementById("AlternativesList"); // retrieves the element from the dedicated element
+      listContainer.innerHTML = ""; // Erase previous content
+      var list = createAlternativesList(); // create new list - Method to adapt with PhonoWriter source code...
+      listContainer.appendChild(list); // Display (add) the list
     }
-    // Synchronize the document state with the Office.js runtime
-    return context.sync();*/
   });
 }
 
-export async function select() {
-  return Word.run(async function (context) {
-    var selection = context.document.getSelection();
-    context.load(selection);
-    var html = selection.getHtml();
+// //DOESN'T WORK CORRECTLY FOR NOW
+// function detectWrittenText(eventArgs) {
+//   // Retrieves the text of the last added content control element
+//   var contentControl = eventArgs.contentControl;
+//   contentControl.addHandlerAsync(Office.EventType.Input, function (eventArgs) {
+//       var text = eventArgs.target.text;
+//       console.log(text);
+//       // display the written text in dedicated text zone
+//       document.getElementById("userWrittenText").innerHTML = text;
+//     });
+// }
+
+function createAlternativesList() {
+  var list = document.createElement("ul");
+  var items = ["Alternative1", "Alternative2", "Alternative3"]; // List 'example'
+  for (var i = 0; i < items.length; i++) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(items[i]));
+    // add listener to replace user's selection by selected word from the list (item)
+    li.addEventListener("dblclick", function () {
+      // document.getElementById("userText").innerHTML = this.innerHTML;
+      replaceText(this.innerHTML);
+    });
+    list.appendChild(li);
+  }
+  return list;
+}
+
+async function replaceText(newText) {
+  await Word.run(async (context) => {
+    const doc = context.document;
+    const originalRange = doc.getSelection();
+    originalRange.insertText(newText, "Replace");
+
     await context.sync();
-    document.getElementById("mySelection").innerHTML = html.value; //Get the selected text in HTML
+  }).catch(function (error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
   });
 }
